c6631c7046f66ac2595ea1a45cf525b5b7639b99
Your Name <you@example.com>
2023-02-20 16:21:28 +0530

    com2

--- api_test.sh
+++ api_test.sh
@@ -1,1 +1,9 @@
-curl --header "Content-Type: application/json"   --request POST   --data '{"username":"xyz","password":"xyz"}' http://localhost:4000/login
+# agent_login
+curl --header "Content-Type: application/json" --request POST --data '{"username":"xyz","password":"xyz"}' http://localhost:4000/login
+
+#master login
+curl --header "Content-Type: application/json" --request POST --data '{"username":"username","password":"password"}' http://localhost:5000/login
+
+
+#create agent
+curl --header "Content-Type: application/json" --request POST --data '{"username":"def","password":"def","token":"qARwjHt9OQo3NKKwON5QSOURT77FNv5rso32r7ukubqdNqKlLzII0PM5EArXXkFi"}' http://localhost:5000/create_agent
